866cd21094982a5c0fc48de21e6407688879e3a2
Hojun-Cho <ghwns6743@gmail.com>
2025-10-26 08:57:17 +0900

    refactor: remove unused internal/ packages
    Remove legacy internal packages not used by main.go:
    - internal/lib/ - old cgo bindings (superseded by direct usage)
    - internal/ui/ - old UI code (replaced by bubbletea)
    - internal/config/ - old config handling
    
    Following Pike/Cox principles: keep code simple and minimal.

--- internal/config/errors.go
+++
@@ -1,8 +0,0 @@
-package config
-
-import "errors"
-
-var (
-	ErrMissingMattermostHost  = errors.New("MATTERMOST_HOST environment variable is required")
-	ErrMissingMattermostToken = errors.New("MATTERMOST_TOKEN environment variable is required")
-)
